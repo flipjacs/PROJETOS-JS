c098769319b200e49e8c0a7fb08714be46c4b767
Luiz Felipe Sousa Neilson <luizfelipeskype@gmail.com>
2025-02-27 11:36:50 -0400

    Adicionei limite de digito e mostrar o numero do imc

--- JAVASCRIPT/imcEX.js
+++ JAVASCRIPT/imcEX.js
@@ -1,23 +1,25 @@
-let H = Number(prompt("Insira sua altura"))
-let P = Number(prompt("Insira seu peso"))
+let H = Number(prompt("Insira sua altura em METROS"))
+let P = Number(prompt("Insira seu peso em KG"))
 let imc = P / (H * H)
+let imcl= imc.toFixed(2)
+
 
 if ( H <=0 & P <=0 ){
   alert ("Por favor, insira dados corretos")
 }
 
 else if (imc < 18.5) {
-  alert("Você está classificado como MAGREZA")
+  alert( imcl +" Você está classificado como MAGREZA" )
 }
 else if (imc < 24.9) {
-  alert("Você é normal")
+  alert("Seu IMC é: " + imcl +", você está classificado como NORMAL")
 }
 else if (imc < 29.9){
-  alert("Você está em SOBREPESO")
+  alert("Seu IMC é: " + imcl +", você está classificado como SOBREPESO")
 }
 else if (imc < 39.9){
-  alert("Você está em OBESDIDADE 1")
+  alert("Seu IMC é: " + imcl +", você está classificado como OBESIDADE 1")
 }
 else if (imc > 40){
-  alert("Você está em OBESDIDADE 2")
+  alert("Seu IMC é: " + imcl +", você está classificado como OBESIDADE 2")
 }
