577575036e67270c63f890c23dc52898fcd8e1aa
Luiz Felipe Sousa Neilson <luizfelipeskype@gmail.com>
2025-02-28 19:52:16 -0400

    Criei um algoritmo para medir média
    Criei algoritmo para media aritmética de notas com sistema de notas inválidas

---
+++ JAVASCRIPT/mAluno.js
@@ -0,0 +1,16 @@
+let AV1 = Number(prompt("Insira a nota da Avaliação 1"))
+let AV2 = Number(prompt("Insira a nota da Avaliação 2"))
+let AV3 = Number(prompt("Insira a nota da Avaliação 3"))
+let media = (AV1 + AV2 + AV3)/3
+if (AV1<0 | AV2<0 | AV3<0 | AV1>10 | AV2>10 | AV3>10){
+  alert ("NOTAS INVÁLIDAS")
+}
+else if (media >= 7){
+  alert ("Você passou de semestre!")
+}
+else if (media <7 & media >= 5){
+  alert ("Você vai fazer a prova final!")
+}
+else if (media <5){
+  alert ("VAI PAGAR MATÉRIA KKKKK")
+}
