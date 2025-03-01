9ab2a00f63577a25db31cab0024f2cc0ba55b56a
Luiz Felipe Sousa Neilson <luizfelipeskype@gmail.com>
2025-02-28 20:10:01 -0400

    Create salarioAumento.js

---
+++ JAVASCRIPT/salarioAumento.js
@@ -0,0 +1,5 @@
+let nomeFuncionario = prompt("Insira seu nome")
+let salario = Number(prompt("Agora insira seu salario"))
+let aumento = salario + (salario*10)/100
+
+alert ( " Parabéns, " + nomeFuncionario + ", agora este é seu salário: " + aumento )
