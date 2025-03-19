632e370a89dbbabcc78273a7700afeb57ac19b93
Luiz Felipe Sousa Neilson <luizfelipeskype@gmail.com>
2025-03-19 13:17:30 -0400

    Update salarioAumento.js

--- JAVASCRIPT/salarioAumento.js
+++ JAVASCRIPT/salarioAumento.js
@@ -1,5 +1,6 @@
 let nomeFuncionario = prompt("Insira seu nome")
 let salario = Number(prompt("Agora insira seu salario"))
-let aumento = salario + (salario*10)/100
+let porcentagemAumento = Number(prompt("Insira a porcentagem de aumento"))
+let aumento = salario + (salario*porcentagemAumento)/100
 
 alert ( " Parabéns, " + nomeFuncionario + ", agora este é seu salário: " + aumento )
